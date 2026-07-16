b0ca861e33674ead270833701c51785bae960987
Nguyễn Xuân Hùng - PS50057 <hungnguyenxuan668@gmail.com>
2026-07-16 11:29:09 +0700

    up bài mới
    thêm slder 3

---
+++ NMTL-NguyenXuanHung-PS50057-main/lab 2/slider 3/GiaiPTB1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    float x;
+    printf("Nhap a,b: ");
+    scanf("%d%d", &a, &b);
+    if (a==0)
+    {
+        printf("Phuong trinh vo nghiem");
+    }
+    else
+    {
+        x = (float)(-b) / a;
+        printf("Phuong trinh %dx + %d = 0 co nghiem x: %.2f\n",a,b,x);
+    }
+    return 0;
+}
