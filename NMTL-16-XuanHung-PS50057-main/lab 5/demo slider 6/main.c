a7479aad74e95c78dd33f38065dc9ae5be85d4e0
Nguyễn Xuân Hùng - PS50057 <hungnguyenxuan668@gmail.com>
2026-08-01 10:18:12 +0700

    sider 6
    mảng 1 chiều

---
+++ NMTL-16-XuanHung-PS50057-main/lab 5/demo slider 6/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+ int B[5] = {3,6,7,2,4};
+//khai báo mảng B
+//kích thước mảng B là 5 phần tử
+//Gán giá trị cho các phần tử của mảng B
+ float D[10];
+ for(int i = 0; i < 5; i++)
+ {
+    printf("B[%d] = %d\n", i, B[i]);
+ }
+    return 0;
+}
